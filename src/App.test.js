b78c5e7fb1a6657dbdabcbce0c87ef22c5b70e60
mehdibenyebka <mehdib030@gmail.com>
2020-02-22 19:07:42 -0600

    Adding tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {ReactDOM,unmountComponentAtNode} from 'react-dom'
 import App from './App'
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  //ReactDOM.render(<App />, div)
 })
 
-
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
